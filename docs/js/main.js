115e89bc3f51d201c626a7793306dc997d3983c3
AntonKlimantow <klimantow@yandex.ru>
2018-07-25 22:32:14 +0300

    portfolio end

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,4 +3,14 @@ $(document).ready(function() {
 	$("nav a, a.mouse_scroll").mPageScroll2id({
 	    highlightSelector:"nav a"
 	});
+
+	$('#portfolio-projects').mixItUp();
+
+	$(".fancybox").fancybox({
+		helpers: {
+			overlay: {
+				locked: false
+			}
+		}
+	});
 });
