b1181f0a229388bc55a52875ab071a7914f93ac1
AntonKlimantow <klimantow@yandex.ru>
2018-08-02 22:05:02 +0300

    final

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,4 +3,34 @@ $(document).ready(function() {
 	$("nav a, a.mouse_scroll").mPageScroll2id({
 	    highlightSelector:"nav a"
 	});
+
+	$("#portfolio-projects").mixItUp();
+
+	$(".fancybox").fancybox({
+		helpers: {
+			overlay: {
+				locked: false
+			}
+		}
+	});
+
+	$("#contact-form").validate({
+		rules: {
+			name: { required: true },
+			email: { required: true, email: true },
+			// skype:  { required: true },
+			// phone:  { required: true },
+			message: { required: true }
+		},
+
+		messages: {
+			name: "Пожалуйста, введите свое имя",
+			email: {
+				required: "Пожалуйста, введите свой email",
+				email: "Email адрес должен быть в формате name@domain.com . Возможно вы ввели email с ошибкой."
+			},
+			message: "Пожалуйста, введите текст сообщения"
+		}
+	})
+
 });
